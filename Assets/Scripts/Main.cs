68084f7b8be76604af6aead6717d82469836a457
hecoding <hlaria@ucm.es>
2015-05-23 07:27:38 +0200

    added background, player dynamics and camera constraints

---
+++ Assets/Scripts/Main.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Main : MonoBehaviour {
+
+	// a public reference to the player would be better than take it directly
+	public bool freezeY;
+
+	private Vector3 initialPos;
+	private float currentYPos;
+	private float offsetXPos;
+
+	void Start () {
+		initialPos = transform.position;
+		offsetXPos = transform.position.x - PlayerController.S.transform.position.x;
+	}
+
+	void Update () {
+		if (freezeY) currentYPos = initialPos.y;
+		else 		 currentYPos = PlayerController.S.transform.position.y;
+
+		transform.position = new Vector3 (PlayerController.S.transform.position.x + offsetXPos, currentYPos, initialPos.z);
+	}
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,36 +2,88 @@
 using System.Collections;
 
 public class PlayerController : MonoBehaviour {
+	static public PlayerController S;
+
+	public float initialForce = 250.0f;
+	public float maxAbsVelX = 20.0f;
+	public float maxAbsVelY = 20.0f;
+	public float jumpForce = 250.0f;
+	public float transmittedForceToLiana = 1000.0f;
 
 	private Rigidbody2D rb;
+	private Transform oldParent;
 	private bool hanged;
-	// Use this for initialization
+	private Bounds playerBounds;
+	private float lowerBound;
+	private bool velChanged;
+	private float currentVelX;
+	private float currentVelY;
+
+	void Awake() {
+		S = this;
+	}
+	
 	void Start () {
 		rb = GetComponent<Rigidbody2D> ();
+		oldParent = transform.parent;
+		lowerBound = Camera.main.ScreenToWorldPoint (new Vector3 (0, 0, 0)).y;
+		velChanged = false;
+		currentVelX = currentVelY = 0f;
 
-		rb.AddForce (new Vector2(2, 1) * 250.0f);
-		Debug.Log ("hola");
+		rb.AddForce (new Vector2(2, 1) * initialForce);
 	}
-	
-	// Update is called once per frame
+
 	void Update () {
-	
+
 	}
 
-	void OnTriggerEnter2D (Collider2D other)  {
-		Debug.Log ("colision");
+	void FixedUpdate () {
+		if (hanged && Input.GetKeyUp ("space")) {
+			rb.isKinematic = false;
+			rb.AddForce (new Vector2(2, 1) * jumpForce);
+			transform.rotation = Quaternion.Euler(0,0,0);
+			// some inverse force to the liana would be nice
 
-		if (!hanged) {
-			other.attachedRigidbody.velocity = rb.velocity;
-			//rb.velocity = Vector2.zero;
-			transform.parent = other.transform;
-			//Destroy (this.gameObject);
-			hanged = true;
+			transform.parent = oldParent;
+			hanged = false;
+		}
+
+		if (transform.position.y < lowerBound) {
+			// do whatever it has to be done when player die
+			transform.position = new Vector3(-5.92f,-2.35f,0f);
+		}
+
+		currentVelX = rb.velocity.x;
+		currentVelY = rb.velocity.y;
+
+		if (Mathf.Abs (rb.velocity.x) > maxAbsVelX) {
+			currentVelX = maxAbsVelX * Mathf.Sign(rb.velocity.x);
+			velChanged = true;
+		}
+		if (Mathf.Abs (rb.velocity.y) > maxAbsVelY) {
+			currentVelY = maxAbsVelY * Mathf.Sign(rb.velocity.y);
+			velChanged = true;
+		}
+
+		if (velChanged) {
+			rb.velocity = new Vector2 (currentVelX, currentVelY);
+			velChanged = false;
 		}
 	}
 
-	void OnTriggerExit2D (Collider2D other) {
+	void OnTriggerEnter2D (Collider2D other)  {
+		switch (other.tag) {
+		case "Liana": 
+			if (!hanged && Input.GetKey("space")) {
+				other.attachedRigidbody.AddForce(new Vector2(1,0) * transmittedForceToLiana);
 
-		hanged = false;
+				rb.isKinematic = true;
+				transform.parent = other.transform;
+
+				hanged = true;
+			}
+
+			break;
+		}
 	}
 }
